1bdcd0ce269db7b1ccb0952301674a83c060a9bd
DaliborHudak <daliborhudak123456798@gmail.com>
2024-03-11 22:38:21 +0100

    dashboard blade looks cleaner now

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,7 +6,27 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    public function index() {
-        return view('welcome');
+    public function index()
+    {
+
+        $users = [
+            [
+
+
+                'name' => 'Alex',
+                'age' => 30,
+            ],
+            [
+                'name' => 'Dan',
+                'age' => 25,
+            ]
+        ];
+
+        return view(
+            'dashboard',
+            [
+                'users' => $users
+            ]
+        );
     }
 }
