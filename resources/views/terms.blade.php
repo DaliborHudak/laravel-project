be58ba59142ca6119856237919a35d97282731d3
DaliborHudak <daliborhudak123456798@gmail.com>
2024-03-14 22:18:17 +0100

    maded layout folder with blades for navbar & footer and layout (doctype with links etc..)

---
+++ resources/views/terms.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.layout')
+@section('content')
+    
+<h1>Terms</h1>
+<div>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto tenetur illum perferendis. At, in? Repellendus,
+    veritatis architecto exercitationem nesciunt delectus deserunt consequatur. Similique, eius in doloremque quae nulla
+    quaerat harum.
+</div>
+
+@endsection
