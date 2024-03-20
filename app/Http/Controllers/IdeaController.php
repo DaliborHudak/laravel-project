5a47e01482ac71388553e7ab0e51ec069c66400d
DaliborHudak <daliborhudak123456798@gmail.com>
2024-03-20 15:20:01 +0100

    update functionality (crud)

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -7,28 +7,53 @@ use Illuminate\Http\Request;
 
 class IdeaController extends Controller
 {
+    
+
     public function show(Idea $idea){
         return view('ideas.show', compact('idea'));
     }
+
 
     public function store(){
 
         request()->validate([
             // min 3 characters, max 240 (idea validation)
-            'idea' => 'required|min:3|max:240'
+            'content' => 'required|min:3|max:240'
         ]);
 
         $idea = Idea::create(
             [
-            'content' => request()->get('idea', ''),
+            'content' => request()->get('content', ''),
         ]
     );
+
         return redirect()->route('dashboard')->with('success', 'Idea was created successfully!');
     }
+
 
     public function destroy(Idea $idea){
         $idea->delete();
 
         return redirect()->route('dashboard')->with('success', 'Idea was deleted successfully!');
+    }
+
+
+    public function edit(Idea $idea){
+        $editing = true;
+        return view('ideas.show', compact('idea', 'editing'));
+    }
+
+
+    public function update(Idea $idea){
+        
+        request()->validate([
+            // min 3 characters, max 240 (idea validation)
+            'content' => 'required|min:3|max:240'
+        ]);
+
+        $idea->content = request()->get('content', '');
+        $idea->save();
+
+        return redirect()->route('ideas.show', $idea->id)->with('success', "Idea updated successfully!");
     }
 }
