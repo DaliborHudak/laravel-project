bf8f02a2579a070849f5b364c4b62ef784ba08d1
DaliborHudak <daliborhudak123456798@gmail.com>
2024-03-25 15:03:44 +0100

    implemented my own login/registration functionality to know how laravel breeze or jetstream etc.. works

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -36,4 +36,38 @@ class AuthController extends Controller
         // redirect
         return redirect()->route('dashboard')->with('success', 'Account created Successfully!');
     }
+
+    public function login()
+    {
+        return view('auth.login');
+    }
+
+    public function authenticate()
+    {
+        $validated = request()->validate(
+            [
+                'email' => 'required|email',
+                'password' => 'required|min:8'
+            ]
+        );
+
+        if(auth()->attempt($validated)){
+            request()->session()->regenerate();
+
+            return redirect()->route('dashboard')->with('success', 'Logged in Successfully!');
+        }
+
+        return redirect()->route('login')->withErrors([
+            'email' => "Matching user not found with the provided email and password"
+        ]);
+    }
+
+    public function logout(){
+        auth()->logout();
+
+        request()->session()->invalidate();
+        request()->session()->regenerateToken();
+
+        return redirect()->route('dashboard')->with('success', 'Logged out Successfully!');
+    }
 }
