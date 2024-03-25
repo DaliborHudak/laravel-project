4173dfb28c8a141c3a97c493e966b0641baaae70
DaliborHudak <daliborhudak123456798@gmail.com>
2024-03-25 13:39:25 +0100

    created simply registration & error messages for fields

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,15 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    public function register(){
+    public function register()
+    {
         return view('auth.register');
     }
 
-    public function store(){
+    public function store()
+    {
+        // validation
+        $validated = request()->validate(
+            [
+                'name' => 'required|min:3|max:40',
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required|confirmed|min:8'
+            ]
+        );
 
+        // create the user
+        User::create(
+            [
+                'name' => $validated['name'],
+                'email' => $validated['email'],
+                'password' => Hash::make($validated['password']),
+            ]
+        );
+
+        // redirect
+        return redirect()->route('dashboard')->with('success', 'Account created Successfully!');
     }
 }
