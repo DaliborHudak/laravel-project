90259a6b62666424a69fe65fcf6bfdbc350571d2
DaliborHudak <daliborhudak123456798@gmail.com>
2024-03-25 12:49:26 +0100

    in the terms of security I've tried to learn the basics of  and

--- app/Http/Controllers/IdeaController.php
+++ app/Http/Controllers/IdeaController.php
@@ -17,18 +17,14 @@ class IdeaController extends Controller
 
     public function store(){
 
-        request()->validate([
+        $validated = request()->validate([
             // min 3 characters, max 240 (idea validation)
             'content' => 'required|min:3|max:240'
         ]);
 
-        $idea = Idea::create(
-            [
-            'content' => request()->get('content', ''),
-        ]
-    );
-
-        return redirect()->route('dashboard')->with('success', 'Idea was created successfully!');
+        Idea::create($validated);
+            
+            return redirect()->route('dashboard')->with('success', 'Idea was created successfully!');
     }
 
 
@@ -47,13 +43,12 @@ class IdeaController extends Controller
 
     public function update(Idea $idea){
         
-        request()->validate([
+        $validated = request()->validate([
             // min 3 characters, max 240 (idea validation)
             'content' => 'required|min:3|max:240'
         ]);
 
-        $idea->content = request()->get('content', '');
-        $idea->save();
+        $idea->update($validated);
 
         return redirect()->route('ideas.show', $idea->id)->with('success', "Idea updated successfully!");
     }
