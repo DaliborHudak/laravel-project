51d113816e958353537a951bd719406fd5a0afeb
DaliborHudak <daliborhudak123456798@gmail.com>
2024-03-27 23:03:10 +0100

    init

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -9,11 +9,15 @@ use Illuminate\Http\Request;
 class CommentController extends Controller
 {
     public function store(Idea $idea){
-        $comment = new Comment();
-        $comment->idea_id = $idea->id;
-        $comment->user_id = auth()->id();
-        $comment->content = request()->get('content');
-        $comment->save();
+        $validated = request()->validate([
+            'content' => 'required|min:3|max:240'
+        ]);
+        
+        $validated['user_id'] = auth()->id();
+        $validated['idea_id'] = $idea->id;
+
+
+        Comment::create($validated);
 
         return redirect()->route('ideas.show', $idea->id)->with('success',"Comment posted successfully!");
     }
