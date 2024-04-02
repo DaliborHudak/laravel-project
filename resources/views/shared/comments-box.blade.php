41d5278a1381f85157b52a3eea24e66b835311cf
DaliborHudak <daliborhudak123456798@gmail.com>
2024-04-02 15:41:46 +0200

    setup profile page and edit blades, fixed comment issue

--- resources/views/shared/comments-box.blade.php
+++ resources/views/shared/comments-box.blade.php
@@ -1,4 +1,5 @@
 <div>
+    @auth
     <form action="{{ route('ideas.comments.store', $idea->id) }}" method="POST">
         @csrf
         <div class="mb-3">
@@ -8,25 +9,23 @@
             <button class="btn btn-primary btn-sm"> Post Comment </button>
         </div>
     </form>
+    @endauth
     <hr>
-    {{-- <div class="d-flex align-items-start">
+    @foreach ($idea->comments as $comment)
+    
+    <div class="d-flex align-items-start">
         <img style="width:35px" class="me-2 avatar-sm rounded-circle"
-            src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=Luigi" alt="Luigi Avatar">
+            src="https://api.dicebear.com/6.x/fun-emoji/svg?seed=Luigi" alt="{{ $comment->user->name }}">
         <div class="w-100">
             <div class="d-flex justify-content-between">
-                <h6 class="">Luigi
+                <h6 class="">{{ $comment->user->name }}
                 </h6>
-                <small class="fs-6 fw-light text-muted"> 3 hour
-                    ago</small>
+                <small class="fs-6 fw-light text-muted">{{ $comment->created_at }}</small>
             </div>
             <p class="fs-6 mt-3 fw-light">
-                and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and
-                Evil)
-                by
-                Cicero, written in 45 BC. This book is a treatise on the theory of ethics,
-                very
-                popular during the Renaissan
+                {{ $comment->content }}
             </p>
         </div>
-    </div> --}}
+    </div>
+    @endforeach
 </div>
