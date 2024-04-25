3ab3106473befe5a0b2b8a33ee82edf08e7f39f2
DaliborHudak <daliborhudak123456798@gmail.com>
2024-04-25 14:36:34 +0200

    Active menu links &  left-sidebar changes

--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,11 +1,22 @@
 @extends('layout.layout')
 @section('content')
-    
-<h1>Terms</h1>
-<div>
-    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto tenetur illum perferendis. At, in? Repellendus,
-    veritatis architecto exercitationem nesciunt delectus deserunt consequatur. Similique, eius in doloremque quae nulla
-    quaerat harum.
-</div>
-
+    <div class="row">
+        <div class="col-3">
+            @include('shared.left-sidebar')
+        </div>
+        <div class="col-6">
+            <h1>Terms</h1>
+            <div>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iusto tenetur illum perferendis. At, in?
+                Repellendus,
+                veritatis architecto exercitationem nesciunt delectus deserunt consequatur. Similique, eius in doloremque
+                quae nulla
+                quaerat harum.
+            </div>
+        </div>
+        <div class="col-3">
+            @include('shared.search-bar')
+            @include('shared.follow-box')
+        </div>
+    </div>
 @endsection
